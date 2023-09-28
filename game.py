cac49ff15c62e88a34f741496da6f6c07bd44cca
Aayush Jain <aayushjn11@gmail.com>
2023-09-28 17:41:35 +0530

    refactor: Print alive soldiers

--- game.py
+++ game.py
@@ -377,6 +377,7 @@ class Commander(Soldier):
             set(range(0, self.num_soldiers)) - {soldier["sid"] for soldier in self.alive_soldiers} - {self.sid},
         )
         self.console.print(
+            f"Alive Soldiers: [{COLOR_BLUE}]{self.alive_soldiers}[/{COLOR_BLUE}]]",
             f"Dead Soldiers: [{COLOR_BLUE}]{dead_soldiers}[/{COLOR_BLUE}]",
             f"Current Time: [{COLOR_BLUE}]{self.cur_time}[/{COLOR_BLUE}]",
             f"Total Time: [{COLOR_BLUE}]{self.game_time}[/{COLOR_BLUE}]",
